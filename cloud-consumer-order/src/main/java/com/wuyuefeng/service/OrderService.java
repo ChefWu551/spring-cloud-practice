2810ad7c10456d617af1903c05049ee197055d48
wuyuefeng <565948592@qq.com>
2021-07-12 07:08:57 +0800

    增加restTemplate调用

--- cloud-consumer-order/src/main/java/com/wuyuefeng/service/OrderService.java
+++ cloud-consumer-order/src/main/java/com/wuyuefeng/service/OrderService.java
@@ -1,14 +1,21 @@
 package com.wuyuefeng.service;
 
+import com.alibaba.fastjson.JSONObject;
 import com.wuyuefeng.dao.OrderDao;
 import com.wuyuefeng.model.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 
 @Service
 public class OrderService {
+
+    private static final String PAYMENT_URL = "http://localhost:8081/pay/money?accountId=1&money=";
+
+    @Autowired
+    RestTemplate restTemplate;
 
     @Resource
     OrderDao orderDao;
@@ -18,6 +25,11 @@ public class OrderService {
     }
 
     public Integer createOrder(Order order) {
-        return orderDao.insertOrder(order);
+        int payMoney = order.getNum() * order.getPrice().intValue() * 100;
+        JSONObject res = restTemplate.getForObject(PAYMENT_URL + payMoney, JSONObject.class);
+        Integer code = (Integer) res.get("code");
+        if (code == 200) return orderDao.insertOrder(order);
+
+        return 0;
     }
 }

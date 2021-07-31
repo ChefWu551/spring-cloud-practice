7c8d3334e39a2198b70a8dbb5599e11abef498d8
wuyuefeng <565948592@qq.com>
2021-07-31 19:00:52 +0800

    add gateway filter

---
+++ cloud-gateway-1/src/main/java/com/yuefeng/filter/CustomerGlobalFilter.java
@@ -0,0 +1,32 @@
+package com.yuefeng.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+
+@Component
+@Slf4j
+public class CustomerGlobalFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String username = exchange.getRequest().getQueryParams().getFirst("username");
+        if (username == null) {
+            log.error("缺少必要的参数");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
